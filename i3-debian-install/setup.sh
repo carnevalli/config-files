9b7deebf14afb3537a5fd42c8c3fbe02b7acaed3
carnevalli <carnevalli@debian-BULLSEYE-live-builder-AMD64>
2023-09-15 01:06:42 -0300

    uses brightnessctl instead of xbacklight

--- i3-debian-install/setup.sh
+++ i3-debian-install/setup.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 
 apt install i3 pipewire pipewire-pulse pavucontrol libspa-0.2-bluetooth \
-    pulseaudio-utils blueman xbacklight playerctl scrot
+    pulseaudio-utils blueman brightnessctl playerctl scrot
